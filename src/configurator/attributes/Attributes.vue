4f50c8386044a8f1e3dec3d5fddbfc3d3c77b95b
Aziz Deghais <deghaisaziz@gmail.com>
2024-05-16 14:50:45 +0200

    feat: root wrapper for attributes

--- src/configurator/attributes/Attributes.vue
+++ src/configurator/attributes/Attributes.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
 import AttributeItem from "./AttributeItem.vue";
-
+import Root from "../../components/Root.vue";
 export default {
     components: {
         AttributeItem
@@ -9,7 +9,21 @@ export default {
 </script>
 
 <template>
-    <div>
+    <Root class="attributes-root">
         <AttributeItem :attributes="['SalesRegion', 'Construction', 'Transmission', 'TransmissionType', 'EngineType', 'HeavyDuty', 'HorsePower', 'Accessories']"></AttributeItem>
-    </div>
+    </Root>
 </template>
+
+<style scoped>
+.attributes-root{
+    grid-area: attributes;
+    display: grid;
+    grid-auto-flow: row;
+    align-content: start;
+    gap: 1em;
+    background-color: var(--color-base-1);
+    padding: var(--size-card-padding);
+    border-radius: var(--shape-card-border-radius);
+    box-shadow: var(--shadow-card);
+}
+</style>
