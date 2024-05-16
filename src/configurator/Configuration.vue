2b4967ae9f76294df2a2442e8b06de9eb4b1bc20
Aziz Deghais <deghaisaziz@gmail.com>
2024-05-16 17:53:19 +0200

    feat: Configuration.vue

---
+++ src/configurator/Configuration.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import {  provide } from 'vue';
+import { useConfigurationManagement } from '../utils/useConfigurationManagement'
+import {
+  AllowedInExplain,
+  AttributeRelations,
+  ConfigurationModelSource,
+  IConfiguratorClient,
+  SessionContext,
+} from "@viamedici-spc/configurator-ts";
+
+const props = defineProps({
+    configuratorClient: Object as () => IConfiguratorClient,
+    configurationModelSource: Object as () => ConfigurationModelSource,
+    attributeRelations: Object as () => AttributeRelations,
+    usageRuleParameters: Object as () => Record<string, string>,
+    allowedInExplain: Object as () => AllowedInExplain,
+});
+
+const { configurationClient, ...sessionContext} = props;
+
+useConfigurationManagement({
+    configurationClient,
+    sessionContext
+})
+</script>
+
+<script lang="ts">
+export default {
+  name: "Configuration",
+};
+</script>
+
+<template>
+  <div>Configuration</div>
+  <!-- slots to recognize and accept children props-->
+  <slot></slot>
+</template>
