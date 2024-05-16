f4f157cdd00c9163abc19996d0870cc08e7fa9dc
Aziz Deghais <deghaisaziz@gmail.com>
2024-05-16 15:29:55 +0200

    feat: ErrorIndicator component

---
+++ src/configurator/ErrorIndicator.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import Root from '../components/Root.vue';
+</script>
+
+<template>
+<Root class="errorIndicator-root">
+    <div class="errorIndicator-title">
+       Initialization Error
+    </div>
+    <p>
+        Error type: Error
+    </p>
+    <button>Retry</button>
+</Root>
+</template>
+
+<style scoped>
+.errorIndicator-root {
+    grid-area: error-indicator;
+    color: hsl(0, 0%, 91%);
+    background-color: var(--color-error);
+    padding: var(--size-card-padding);
+    border-radius: var(--shape-card-border-radius);
+    margin-bottom: 1.5em;
+    box-shadow: var(--shadow-card);
+}
+.errorIndicator-title{
+    margin-top: 0;
+}
+</style>
