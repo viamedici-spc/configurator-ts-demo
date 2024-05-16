3a8ef6a8838b9ae0ea30c0c620595078c0b765f2
Aziz Deghais <deghaisaziz@gmail.com>
2024-05-16 13:59:59 +0200

    feat: Root Component Wrapper

--- src/Root.vue
+++ src/Root.vue
@@ -1,15 +1,12 @@
-<style scoped>
- .root{
-    height: 100vh;
-    padding-left: 1em;
-    padding-right: 1em;
-    padding-bottom: 1em;
-    display: flex;
-    justify-content: center;
- }
-   
-</style>
-
 <template>
-    <div></div>
+<div>
+    <slot></slot>
+</div>
 </template>
+
+  
+<script>
+export default {
+    name: 'Root'
+}
+</script>
