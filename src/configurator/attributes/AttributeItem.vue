67862f729e0b878da70603e8b43f44851b45f64a
Aziz Deghais <deghaisaziz@gmail.com>
2024-05-16 15:02:15 +0200

    feat: label inside AttributeItem

--- src/configurator/attributes/AttributeItem.vue
+++ src/configurator/attributes/AttributeItem.vue
@@ -1,7 +1,13 @@
 <script lang="ts">
 import Root from '../../components/Root.vue';
+import Label from '../../components/Label.vue';
+
 export default {
     name: 'AttributeItem',
+    components: {
+        Root,
+        Label
+    },
     props: {
         attributes: {
             type: Array,
@@ -12,15 +18,20 @@ export default {
 </script>
 
 <template>
-    <Root class="attributeItem-root">
-        <p v-for="attribute in attributes" :key="attribute">{{ attribute }}</p>
-    </Root>
+<Root class="attributeItem-root">
+    <Label> Label of Attribute</Label>
+    <p v-for="attribute in attributes" :key="attribute">{{ attribute }}</p>
+</Root>
 </template>
 
 <style scoped>
-.attributeItem-root{
+.attributeItem-root {
     display: grid;
     grid-template-columns: [label] 0.4fr 0.5em [selection] 0.6fr;
     grid-template-rows: [label selection] auto;
+}
+
+.attributeItem-label {
+    grid-area: label;
 }
 </style>
