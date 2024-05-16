eab688a79699b3c54c9db7c907d8fe8bfbe24809
Aziz Deghais <deghaisaziz@gmail.com>
2024-05-16 15:01:57 +0200

    style: whitespace

--- src/components/Label.vue
+++ src/components/Label.vue
@@ -1,13 +1,12 @@
 <template>
-    <div>
-        <slot></slot>
-    </div>
-    </template>
+<div>
+    <slot></slot>
+</div>
+</template>
+
     
-      
-    <script>
-    export default {
-        name: 'Label'
-    }
-    </script>
-    
+<script>
+export default {
+    name: 'Label'
+}
+</script>
