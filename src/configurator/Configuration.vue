cafdaf90153b839dc155c6343db068a012acc393
Aziz Deghais <deghaisaziz@gmail.com>
2024-05-17 15:43:58 +0200

    feat: reference on configuration from sessions + context is provided

--- src/configurator/Configuration.vue
+++ src/configurator/Configuration.vue
@@ -5,31 +5,41 @@
 </template>
 
 <script setup lang="ts">
-import {  provide, ref, onMounted } from "vue";
+import { provide, ref, onMounted } from "vue";
 import {
+  ConfigurationContextKey,
+  provideConfigurationContext,
+} from "../utils/contexts";
+import {
+  Configuration,
   ConfigurationModelSourceType,
   createClient,
 } from "@viamedici-spc/configurator-ts";
 import * as config from "../config";
-import { createLogger } from "vite";
-onMounted(() => {
-  console.log("Mounted");
+
+const configuration = ref<Configuration | null>(null);
+
+onMounted( async () => {
+  const client = createClient({
+    sessionHandler: {
+      accessToken: config.hcaEngineAccessToken,
+    },
+    hcaEngineBaseUrl: config.hcaEngineEndpoint,
+  });
+
+  const session = await client.createSession({
+    configurationModelSource: {
+      type: ConfigurationModelSourceType.Channel,
+      deploymentName: config.configurationModelPackage.deploymentName,
+      channel: config.configurationModelPackage.channel,
+    },
+  });
+
+  configuration.value = session.getConfiguration();
+
+  console.log(session.getConfiguration());
+
 });
 
-const client = createClient({
-  sessionHandler: { accessToken: config.hcaEngineAccessToken },
-  hcaEngineBaseUrl: config.hcaEngineEndpoint,
-});
-
-const session = await client.createSession({
-  configurationModelSource: {
-    type: ConfigurationModelSourceType.Channel,
-    deploymentName: config.configurationModelPackage.deploymentName,
-    channel: config.configurationModelPackage.channel,
-  },
-});
-
-console.log(session.getConfiguration())
-
-// Provide context values
+provideConfigurationContext(configuration);
 </script>
