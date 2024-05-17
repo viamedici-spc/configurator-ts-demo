ea12390b0fbd07ac992f71b8deab173856dcf9a7
Aziz Deghais <deghaisaziz@gmail.com>
2024-05-17 16:40:12 +0200

    feat : AttributeTypeSelector with "attribute" prop

--- src/configurator/attributes/AttributeItem.vue
+++ src/configurator/attributes/AttributeItem.vue
@@ -1,44 +1,89 @@
-<script lang="ts">
-import Root from '../../components/Root.vue';
-import Label from '../../components/Label.vue';
-import { PropType, defineComponent } from 'vue';
+<template>
+  <Root class="attributeItem-root">
+    <Label>Label of Attribute</Label>
+    <p>{{ attributeId.localId }}</p>
+    <!--"attribute" should be called from useAttributes just as in the react demo -->
+    <AttributeTypeSelector :attribute="attribute" />
+  </Root>
+</template>
 
-interface AttributeId{
-    localId: string,
-    [key: string]: any;
+<script lang="ts">
+import { Attribute, AttributeType } from "@viamedici-spc/configurator-ts";
+import Root from "../../components/Root.vue";
+import Label from "../../components/Label.vue";
+import { PropType, defineComponent, h } from "vue";
+import ComponentAttribute from "./component/ComponentAttribute.vue";
+import ChoiceAttribute from "./choice/ChoiceAttribute.vue";
+import BooleanAttribute from "./boolean/BooleanAttribute.vue";
+import NumericAttribute from "./numeric/NumericAttribute.vue";
+
+interface AttributeId {
+  localId: string;
+  [key: string]: any;
 }
 
-export default defineComponent({
-    name: 'AttributeItem',
-    components: {
-        Root,
-        Label
+const AttributeTypeSelector = defineComponent({
+  props: {
+    attribute: {
+      type: Object as PropType<Attribute>,
+      required: true,
     },
-    props: {
-        attributeId: {
-            type: Object as PropType<AttributeId>,
-            required: true
-        }
-    }
-})
+  },
+  components: {
+    ComponentAttribute,
+    ChoiceAttribute,
+    BooleanAttribute,
+    NumericAttribute,
+  },
+  setup(props) {
+    return () => {
+      switch (props.attribute?.type) {
+        case AttributeType.Choice:
+          return h(ChoiceAttribute);
+        case AttributeType.Numeric:
+          return h(NumericAttribute);
+        case AttributeType.Boolean:
+          return h(BooleanAttribute);
+        case AttributeType.Component:
+          return h(ComponentAttribute);
+        case null:
+        case undefined: 
+        return h('span', 'Attribute not found !')
+        default:
+        return h('span', 'Unknown attribute type')
+      }
+    };
+  },
+});
 
+export default defineComponent({
+  name: "AttributeItem",
+  components: {
+    Root,
+    Label,
+    AttributeTypeSelector,
+  },
+  props: {
+    attributeId: {
+      type: Object as PropType<AttributeId>,
+      required: true,
+    },
+    attribute: {
+      type: Object as PropType<Attribute>,
+      required: true,
+    },
+  },
+});
 </script>
-
-<template>
-<Root class="attributeItem-root">
-    <Label> Label of Attribute</Label>
-    <p>{{ attributeId.localId}}</p>
-</Root>
-</template>
 
 <style scoped>
 .attributeItem-root {
-    display: grid;
-    grid-template-columns: [label] 0.4fr 0.5em [selection] 0.6fr;
-    grid-template-rows: [label selection] auto;
+  display: grid;
+  grid-template-columns: [label] 0.4fr 0.5em [selection] 0.6fr;
+  grid-template-rows: [label selection] auto;
 }
 
 .attributeItem-label {
-    grid-area: label;
+  grid-area: label;
 }
 </style>
