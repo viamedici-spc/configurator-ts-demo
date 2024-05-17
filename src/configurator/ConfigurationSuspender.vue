5e7496eec335ba437d29ce450de28cd3f08e6d4d
Aziz Deghais <deghaisaziz@gmail.com>
2024-05-17 15:44:16 +0200

    style: commented

---
+++ src/configurator/ConfigurationSuspender.vue
@@ -0,0 +1,14 @@
+<!-- <template>
+  <slot></slot>
+</template>
+
+<script setup lang="ts">
+import { useConfigurationInitialization } from '../utils/ConfigurationLifeCylce';
+
+
+const { isInitializingPromise } = useConfigurationInitialization();
+
+if (isInitializingPromise?.promise) {
+  throw isInitializingPromise.promise;
+}
+</script> -->
