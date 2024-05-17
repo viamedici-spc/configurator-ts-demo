f45e84d6a62006e23617462754f3e90019ba034f
Aziz Deghais <deghaisaziz@gmail.com>
2024-05-17 17:19:43 +0200

    feat: types of attributes added as templates

---
+++ src/configurator/attributes/numeric/NumericAttribute.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="root">
+   NumericAttribute
+</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.root {
+    grid-area: selection;
+    display: grid;
+    grid-template-rows: [value-selection] auto [gap] 0.2em [indicators] auto;
+    grid-template-columns: [value-selection indicators] 1fr;
+}
+
+.indicators {
+    grid-area: indicators;
+    font-size: 0.8em;
+}
+</style>
