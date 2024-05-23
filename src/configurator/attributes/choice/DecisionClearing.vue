96556c060cc248db5537f34366bc19e1820e4dfa
Aziz Deghais <deghaisaziz@gmail.com>
2024-05-23 16:09:04 +0200

    feat: decisionClearing

---
+++ src/configurator/attributes/choice/DecisionClearing.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="root">
+        <button @click="onClick">Clear decisions</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useChoiceAttribute } from '../../../utils/useAttributes';
+import { useActiveAttribute } from '../AttributeItem.vue';
+
+ const activeAttribute = useActiveAttribute();
+ const attribute = useChoiceAttribute(activeAttribute!);
+ const clearDecisions = useChoiceAttribute(activeAttribute!);
+
+ const onClick = async()=>{
+    alert(clearDecisions?.explain);
+ }
+
+
+</script>
+
+<style scoped>
+.root{
+    grid-area: label;
+    display: flex;
+    justify-content: end;
+    align-self: start;
+}
+</style>
