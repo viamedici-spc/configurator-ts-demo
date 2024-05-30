e30f248e7a4a583581449d2daa01e0a043111f70
Aziz Deghais <deghaisaziz@gmail.com>
2024-05-30 15:09:39 +0200

    fix: eliminate vuex as a store used for re-rendering

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,5 @@
 import { createApp } from 'vue'
-import { createStore } from 'vuex'
 import App from './App.vue'
 import './globalStyles.css'
 
-const store = createStore({
-    state: {
-        rerender: 0,
-    },
-    mutations: {
-        setRerender(state) {
-            state.rerender = state.rerender + 1;
-        },
-    },
-})
-
-createApp(App).use(store).mount('#app')
+createApp(App).mount('#app')
